07140effd9e49f76c6954c8f494ad753696977ad
burgundythedev <bourgogne.olivier@gmail.com>
2023-09-07 13:28:19 +0200

    fix routes

--- src/components/Admin/AdminRoute/AdminRoute.js
+++ src/components/Admin/AdminRoute/AdminRoute.js
@@ -14,7 +14,7 @@ const AdminRoute = ({ children }) => {
       <p className="admin-route__paragraph">
         Page can only be accessible by admin user
       </p>
-      <Link to="/">
+      <Link to="/E-Commerce-WebSite">
         <button className="admin-route__button">&larr; Back to Home</button>
       </Link>
     </section>
